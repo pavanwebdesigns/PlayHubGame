4a0d0e0a5dfce2fd58cc5926c505dd79812fbd92
pavanwebdesigns <132897355+pavanwebdesigns@users.noreply.github.com>
2025-11-18 17:01:54 +0530

    stable version
    stable version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,12 +17,6 @@ const BlogIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
 );
 
-const LogoIcon = () => (
-  <div className="bg-white text-dark rounded d-flex align-items-center justify-center" style={{ width: '40px', height: '40px', fontWeight: 'bold', fontSize: '1.5rem' }}>
-    P
-  </div>
-);
-
 interface HeaderProps {
   onSearch: (term: string) => void;
   onHome?: () => void;
